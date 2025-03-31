cf7a046088e1898e6bd6b701aebe839726ecb178
amalm23 <115096052+amalm23@users.noreply.github.com>
2025-03-31 10:01:52 +0100

    Adding inspirational quotes

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,3 +1,27 @@
 function generateRandomNumber(num) {
     return Math.floor(Math.random() * num);
 } 
+
+const inspirationQuotes = {
+    subject: [
+        "You're",
+        "You",
+        "Your dreams",
+        "Your hard work",
+        "Your journey"
+    ],
+    action: [
+        "can overcome any challenges",
+        "are destined for greatness",
+        "have the power to change the world",
+        "are stronger than you think",
+        "can achieve anything with persistance"
+    ],
+    advice: [
+        "Believe in yourself!",
+        "Never Give Up!",
+        "Keep pushing forwards!",
+        "Success is just around the corner!",
+        "The best is yet to come!"
+    ]
+};
